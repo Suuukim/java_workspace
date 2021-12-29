7b8986bd7494d5989bf20fd7fbf4b386a6628cac
DESKTOP-R2NKGM0\admin <koreait.2021@gmail.com>
2021-12-29 11:50:33 +0900

    1229 오전수업

---
+++ day2/src/koreait/day2/IntegerVarTest.java
@@ -0,0 +1,14 @@
+package koreait.day2;
+
+public class IntegerVarTest {   //정수를 저장하는 변수 연습합니다.
+    public static void main(String[] args) {
+		
+    	byte n1;
+    	short n2;
+    	int n3;
+    	long n4;
+    	
+
+//byte,short,int,long 소문자로 시작하는 것은 정수 데이터 타입을 표시하는 키워드
+	}
+}
