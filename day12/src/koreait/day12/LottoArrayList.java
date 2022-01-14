514f820ba5f54e73210a91acd9fa2fbb550f7851
DESKTOP-R2NKGM0\admin <47629556+kit-lec@users.noreply.github.com>
2022-01-14 14:38:23 +0900

    0114 14:40 ArrayList

---
+++ day12/src/koreait/day12/LottoArrayList.java
@@ -0,0 +1,10 @@
+package koreait.day12;
+
+public class LottoArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- day12/src/koreait/day12/Member.java
+++ day12/src/koreait/day12/Member.java
@@ -4,6 +4,15 @@ public class Member {
 	int id;
 	String name;
 	
+	public Member() {
+	}
+	
+	public Member(int id,String name) {
+		this.id=id;
+		this.name=name;
+	}
+	
+	
 	void print() {		//이제부터는 이 메소드 대신 toString() 재정의해서 사용합니다.
 		System.out.println("id : " + id + ",name : " + name);
 		
