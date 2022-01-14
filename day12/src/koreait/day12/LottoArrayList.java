926da11da3a26149a9bda3f5090ea4b95b526ed5
DESKTOP-R2NKGM0\admin <47629556+kit-lec@users.noreply.github.com>
2022-01-14 14:45:09 +0900

    0114 14:45 Lotto v2

--- day12/src/koreait/day12/LottoArrayList.java
+++ day12/src/koreait/day12/LottoArrayList.java
@@ -1,10 +1,23 @@
 package koreait.day12;
 
+import java.util.ArrayList;
+
 public class LottoArrayList {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArrayList<Integer> lotto = new ArrayList<>();    //선택된 숫자 저장
+		ArrayList<Integer> balls = new ArrayList<>();	 //공 45개 저장하는 리스트 ->44개 -> 43개...
+		
+		//1. balls리스트에 1~45 숫자를 추가한다.
+		
+		//확인: 출력해보기
+		
+		//2. balls 리스트에서 뽑을 인덱스를 난수 0~44 -> 0~43 -> 0~42 .... -> 0~39 범위에서 반복합니다.
+		
+		//2-1 . 난수인덱스에 해당하는 요소를 lotto 리스트에 추가합니다.
+		//2-2 . 뽑힌 난수 인덱스의 요소는 삭제(제거) 합니다.
+		
+		//3. 최종 lotto 변수가 참조하는 값을 출력합니다.
 	}
 
 }
