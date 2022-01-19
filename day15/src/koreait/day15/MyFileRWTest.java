e298d41f979382580dd1c4ab341d40cac3d606f4
DESKTOP-R2NKGM0\admin <47629556+kit-lec@users.noreply.github.com>
2022-01-19 15:52:31 +0900

    0119 15:50 file read write

---
+++ day15/src/koreait/day15/MyFileRWTest.java
@@ -0,0 +1,13 @@
+package koreait.day15;
+
+public class MyFileRWTest {
+
+	public static void main(String[] args) {
+
+		MyFileRW.fileRead("d:\\dev\\test\\자바테스트.txt");
+		MyFileRW.fileRead("c:\\windows\\directx.log");
+		
+		
+	}
+
+}
