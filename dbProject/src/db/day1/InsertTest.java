307bc0df5744a5dc60367972d06e7c6bf3c20653
kimsohee <koreait.2021@gmail.com>
2022-02-09 15:23:40 +0900

    jdbc insert

--- dbProject/src/db/day1/InsertTest.java
+++ dbProject/src/db/day1/InsertTest.java
@@ -1,14 +1,30 @@
 package db.day1;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class InsertTest {		//오라클 idev 스키마 custom# 테이블에 고객을 추가하는 연습
 								// SQL insert 실행
 	public static void main(String[] args) {
 
-		
 		Connection conn = OracleConnectUtil.connect();
+		System.out.println("main메소드 확인용 : "+conn);
 		
+		String sql="INSERT INTO TBL_CUSTOM# " + 
+				"(CUSTOM_ID, NAME, EMAIL, AGE, REG_DATE) " + 
+				" VALUES('sana', '최사나', 'unknown', 22, sysdate)";
+		
+		
+		//sql 을 실행할 객체 생성
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+		
+			pstmt.execute();
+			pstmt.close();
+		}catch (SQLException e) {
+			System.out.println("SQL 실행 오류 : " + e.getMessage());
+		}	
 		
 	}
 
