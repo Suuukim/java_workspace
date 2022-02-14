5aa7537a7ea6a1f3a8ade58ab75344e030ba3b70
kimsohee <koreait.2021@gmail.com>
2022-02-14 15:11:21 +0900

    0214 외부평가 vo

---
+++ dbProject/src/db/day4/SaleByDao.java
@@ -0,0 +1,18 @@
+package db.day4;
+
+public class SaleByDao {
+// v_by_custno view 에서 select 쿼리 실행하는 메소드 정의
+	
+	//싱글턴 객체 생성코드
+	
+	
+	
+	public void selectByCustno() {
+		//SELECT * FROM V_BY_CUSTNO  쿼리 실행하는 코드
+		
+		
+		
+	}
+	
+	
+}
